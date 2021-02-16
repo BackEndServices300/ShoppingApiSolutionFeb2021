9d7ce5956fbda49bf32b1ceffc5efc8e20d386e8
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-16 10:54:48 -0500

    HTTP Head stuff

--- ShoppingApi/Controllers/DemoController.cs
+++ ShoppingApi/Controllers/DemoController.cs
@@ -33,5 +33,47 @@ namespace ShoppingApi.Controllers
         {
             return Ok(new { Markup = _pricing.Value.Markup });
         }
+
+        [HttpGet("demo/employees/{id:int}")]
+        public ActionResult GetAnEmployee(int id)
+        {
+            // do the database query, return either an employee or a 404
+            if(id % 2 == 0)
+            {
+                return Ok(new { Id = id, Name = "Putintane" });
+            } else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpHead("demo/employees/emails/{emailAddress}")]
+        public ActionResult FindEmployeesWithThatEmailAddress(string emailAddress)
+        {
+            if(emailAddress.EndsWith("@aol.com"))
+            {
+                return Ok(); // look ma! no entity
+            } else
+            {
+                return NotFound();
+            }
+        }
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 120)]
+        [HttpGet("demo/employees/emails/{emailAddress}")]
+        public ActionResult FindEmployeesWithThatEmailAddressReally(string emailAddress)
+        {
+            if (emailAddress.EndsWith("@aol.com"))
+            {
+                var response = new Models.CollectionBase<string>
+                {
+                    Data = new List<string> { emailAddress }
+                };
+                return Ok(response); // look ma! no entity
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
