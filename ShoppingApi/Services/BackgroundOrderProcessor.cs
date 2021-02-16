eccdcc07c2403247b2b19daceb1f43048ef031da
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-16 15:32:53 -0500

    Update BackgroundOrderProcessor.cs

--- ShoppingApi/Services/BackgroundOrderProcessor.cs
+++ ShoppingApi/Services/BackgroundOrderProcessor.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShoppingApi.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +14,37 @@ namespace ShoppingApi.Services
     public class BackgroundOrderProcessor : BackgroundService
     {
         private readonly ILogger<BackgroundOrderProcessor> _logger;
+        private readonly CurbsideOrderChannel _channel;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ISystemTime _systemTime;
 
-        public BackgroundOrderProcessor(ILogger<BackgroundOrderProcessor> logger)
+        public BackgroundOrderProcessor(ILogger<BackgroundOrderProcessor> logger, CurbsideOrderChannel channel, IServiceProvider serviceProvider, ISystemTime systemTime)
         {
             _logger = logger;
+            _channel = channel;
+            _serviceProvider = serviceProvider;
+            _systemTime = systemTime;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while(true)
+           await foreach(var order in _channel.ReadAllAsync())
             {
-                _logger.LogInformation("Doing some work in teh background, yo.");
-                await Task.Delay(1000, stoppingToken);
+                //order.CurbsideOrderId
+                using var scope = _serviceProvider.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<ShoppingDataContext>();
+
+                var savedOrder = await context.CurbsideOrders.SingleOrDefaultAsync(o => o.Id == order.CurbsideOrderId);
+                if(savedOrder == null)
+                {
+                    continue;
+                } else
+                {
+                    var numberOfItems = savedOrder.Items.Split(',').Count();
+                    savedOrder.PickupTimeAssigned = _systemTime.GetCurrent().AddHours(numberOfItems);
+                    await context.SaveChangesAsync();
+                }
+                
             }
         }
         public override Task StopAsync(CancellationToken cancellationToken)
